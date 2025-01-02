2771fc921471d8637d741301074e176dbcd6ec8c
Sato <lutereau.s@gmail.com>
2025-01-02 06:50:55 -0300

    c01

---
+++ PiscineC01/ex07/ft_rev_int_tab.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_ultimate_ft.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sluterea <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/12 17:52:40 by sluterea          #+#    #+#             */
+/*   Updated: 2024/08/12 17:53:01 by sluterea         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_rev_int_tab(int *tab, int size)
+{
+	int	temp;
+	int	start;
+	int	end;
+
+	start = 0;
+	end = size - 1;
+	while (start < end)
+	{
+		temp = tab[start];
+		tab[start] = tab[end];
+		tab[end] = temp;
+		start++;
+		end--;
+	}
+}
---
+++ PiscineC01/ex08/ft_sort_int_tab.c
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_ultimate_ft.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sluterea <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/12 17:52:40 by sluterea          #+#    #+#             */
+/*   Updated: 2024/08/12 17:53:01 by sluterea         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_sort_int_tab(int *tab, int size)
+{
+	int	temp;
+	int	j;
+	int	swapped;
+
+	swapped = 1;
+	while (swapped)
+	{
+		swapped = 0;
+		j = 0;
+		while (j < size - 1)
+		{
+			if (tab[j] > tab[j + 1])
+			{
+				temp = tab[j];
+				tab[j] = tab[j + 1];
+				tab[j + 1] = temp;
+				swapped = 1;
+			}
+			j++;
+		}
+		size--;
+	}
+}
